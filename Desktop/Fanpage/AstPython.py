5dbf8686d98e96e12eaac5527fc488a20bf556ae
Denilson Santos <denilsondasilvasan@server>
2017-09-10 14:02:11 -0300

    Novo arquivo

---
+++ Desktop/Fanpage/AstPython.py
@@ -0,0 +1,13 @@
+
+var = '''
+
+        numero1 = int(input("Digite um numero: "))
+        numero2 = int(input(""Digite outro numero: ))
+
+        print (numero1 + numero2)
+
+'''
+
+
+print (var)
+
