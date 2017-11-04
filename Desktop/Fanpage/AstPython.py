68377e1c3fb779764fbaea5381c8b87f6d9cf294
Dn1lson <denilsondasilvasan@gmail.com>
2017-11-04 10:53:50 -0200

    teste commit

--- Desktop/Fanpage/AstPython.py
+++ Desktop/Fanpage/AstPython.py
@@ -3,7 +3,7 @@ var = '''
 
         numero1 = int(input("Digite um numero: "))
         numero2 = int(input(""Digite outro numero: ))
-
+numero3 = int(input("Digite um terceiro numero")
         print (numero1 + numero2)
 
 '''
